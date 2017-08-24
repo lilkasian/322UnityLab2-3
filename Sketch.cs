b0e6b884efa88f6ef906be7be687c2acb5fe337a
lilkasian <lilkasian@users.noreply.github.com>
2017-08-24 13:02:08 +1200

    Add files via upload

---
+++ Sketch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sketch : MonoBehaviour {
+
+    public GameObject myPreFab;
+
+	// Use this for initialization
+	void Start () {
+
+        int totalCubes = 30;
+        float totalDistance = 2.9f;
+
+        for (int i = 0; i < totalCubes; i++)
+        {
+            float perc = i / (float)totalCubes;
+
+            float sin = Mathf.Sin(perc * Mathf.PI/2);
+
+            float x = 1.8f + sin * totalDistance;
+            float y = 5.0f;
+            float z = 0.0f;
+
+            var newCube = (GameObject)Instantiate(myPreFab, new Vector3(x, y, z), Quaternion.identity);
+            newCube.GetComponent<CubeScript>().SetSize(0.45f * (1.0f - perc));
+            newCube.GetComponent<CubeScript>().rotateSpeed = 0.2f + perc*4.0f;
+
+        }
+
+    }   
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
