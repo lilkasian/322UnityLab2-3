f1621d8b451b1ae0f85143e0c7f52db2950a4260
lilkasian <lilkasian@users.noreply.github.com>
2017-08-27 21:15:53 +1200

    timer function added

--- Sketch.cs
+++ Sketch.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Sketch : MonoBehaviour {
 
     public GameObject myPreFab;
+    public Color lerpedColor = Color.white;
+    public float timer = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -25,16 +27,28 @@ public class Sketch : MonoBehaviour {
             var newCube = (GameObject)Instantiate(myPreFab, new Vector3(x, y, z), Quaternion.identity);
             newCube.GetComponent<CubeScript>().SetSize(0.45f * (1.0f - perc));
             newCube.GetComponent<CubeScript>().rotateSpeed = 0.2f + perc*4.0f;
-		
-	    //Makes All Cubes Blue
-            newCube.GetComponent<Renderer>().material.color = new Color(0, 0, 255);
 
+            //Makes All Cubes Blue
+            //newCube.GetComponent<Renderer>().material.color = new Color(0, 0, 255);
         }
 
     }   
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //lerpedColor = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, 1));
+        timer += Time.deltaTime;
+
+        if (timer >= 1.0f)//change the float value here to change how long it takes to switch.
+        {
+            // pick a random color
+            Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+            // apply it on current object's material
+            myPreFab.GetComponent<Renderer>().material.color = newColor;
+            timer = 0;
+        }
+
+    }
+
+
 }
